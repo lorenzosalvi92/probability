d4c7e5a225f46c8ab4059701a8884f8004fd36ac
lorenzosalvi92 <57567372+lorenzosalvi92@users.noreply.github.com>
2024-03-09 12:30:44 +0100

    Add files via upload

---
+++ red-faces-of-a-cube.py
@@ -0,0 +1,23 @@
+'''
+https://www.cut-the-knot.org/Probability/RedFaces.shtml
+'''
+
+import math
+
+
+def n_cubes_with_red_faces(n):
+    threeRedFaces = 8
+    twoRedFaces = (n-2)*12
+    oneRedFace = 6*(math.pow((n-2),2))
+    zeroRedFaces = math.pow((n-2),3)
+    sumOfSmallCubes = threeRedFaces + twoRedFaces + oneRedFace + zeroRedFaces
+    if math.pow(n,3) == sumOfSmallCubes:
+        print('All good.')
+        return sumOfSmallCubes
+    else:
+        print('Something in the calculation is wrong.')
+        return 
+
+n_cubes_with_red_faces(3)
+
+    
